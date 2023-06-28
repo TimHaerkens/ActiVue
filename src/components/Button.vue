e061df18a8e3ee778723d6c9179550fc2c60312c
Tim Haerkens <timhaerkens@gmail.com>
2023-06-28 16:12:05 +0200

    Bit of styling on the button component

--- src/components/Button.vue
+++ src/components/Button.vue
@@ -7,16 +7,29 @@
 @import '@/assets/variables.scss';
 
 button{
+  cursor:pointer;
+
+  color:white;
+  font-size: 1rem;
+  font-weight:600;
+  font-variant:small-caps;
+
   margin-top: 1rem;
   border-radius: 50rem;
   padding: 1rem;
-  font-size: 1rem;
-  background-color:$brand-secondary;
+  background:$brand-secondary;
 
-  cursor:pointer;
-  transition: opacity 0.2s linear;
+  
   &:hover{
-    opacity:0.8;
+    background: linear-gradient(66deg,$brand-secondary,$brand-primary,$brand-secondary);
+    background-size: 500%;
+    animation: glowing 20s linear infinite;
   }
+}
+
+@keyframes glowing {
+  0% { background-position: 0 0; }
+  50% { background-position: 500% 0; }
+  100% { background-position: 0 0; }
 }
 </style>
